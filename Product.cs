9d24f6b381041dde88284ef18a6979fb236b1535
IbraHeemALBilal <ibraheemataiwi123@gmail.com>
2023-07-24 13:27:45 +0300

    Some improvment for the code

--- Product.cs
+++ Product.cs
@@ -8,9 +8,47 @@ namespace Simple_Inventory_Management_System
 {
     internal class Product
     {
-        public string name { get; set; }
-        public double price { get; set; }
-        public int quantity { get; set; }
+        private string Name;
+        private double Price;
+        private int Quantity;
+
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public double price
+        {
+            get { return Price; }
+            set
+            {
+                if (value >= 0)
+                {
+                    Price = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Price cannot be negative.");
+                }
+            }
+        }
+
+        public int quantity
+        {
+            get { return Quantity; }
+            set
+            {
+                if (value >= 0)
+                {
+                    Quantity = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Quantity cannot be negative.");
+                }
+            }
+        }
         public Product(String name, double price, int quantitiy)
         {
               this.name = name;
