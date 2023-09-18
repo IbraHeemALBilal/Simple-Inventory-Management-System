0faa9e115ee2bca152ae039f0beb694a5c0a75fb
IbraHeemALBilal <ibraheemataiwi123@gmail.com>
2023-09-18 15:26:51 +0300

    Using DataBase by sql , mongo and EF core

---
+++ InventoryDbContext .cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleInventoryManagementSystem
+{
+    public class InventoryDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-2628EB6;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=SimpleInventoryManagmentDB");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasKey(p => p.Name);
+
+        }
+    }
+}
---
+++ InventoryUsingEfCore.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem
+{
+    public class InventoryUsingEfCore : IInventoryManagment
+    {
+        private readonly InventoryDbContext _dbContext;
+
+        public InventoryUsingEfCore()
+        {
+            _dbContext = new InventoryDbContext();
+        }
+        public bool AddProduct(Product product)
+        {
+            try
+            {
+                if (product.IsValid())
+                {
+                    var existingProduct = _dbContext.Products.FirstOrDefault(p => p.Name == product.Name);
+
+                    if (existingProduct != null)
+                    {
+                        existingProduct.Quantity += product.Quantity;
+                    }
+                    else
+                    {
+                        _dbContext.Products.Add(product);
+                    }
+
+                    _dbContext.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                return false;
+            }
+        }
+        public List<Product> FetchAllProducts()
+        {
+            try
+            {
+                return _dbContext.Products.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                return new List<Product>();
+            }
+        }
+        public bool EditProduct(string productName, string newName, decimal newPrice, int newQuantity)
+        {
+            var existingProduct = _dbContext.Products.FirstOrDefault(p => p.Name == productName);
+            if (existingProduct != null)
+            {
+                existingProduct.Price = newPrice;
+                existingProduct.Quantity = newQuantity;
+                existingProduct.Price= newPrice;
+                _dbContext.SaveChanges();
+                return true;
+            }
+            else return false;
+        }
+        public bool DeleteProduct(string productName)
+        {
+            var existingProduct = _dbContext.Products.FirstOrDefault(p => p.Name == productName);
+            if (existingProduct != null)
+            {
+                _dbContext.Products.Remove(existingProduct);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            else return false; 
+        }
+        public Product SearchForProduct(string productToDeleteName)
+        {
+            return _dbContext.Products.FirstOrDefault(p => p.Name == productToDeleteName); ;
+        }
+    }
+
+}
